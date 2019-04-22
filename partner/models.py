71d7add1cf681615449361283029bb08fdf50c29
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2019-04-22 19:02:22 +0100

    Finished with partner module but having image not subscriptable error

--- partner/models.py
+++ partner/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.conf import settings
-import re
-from Lists import Tag
+from core.models import User
 from post.models import Post
 
 
 # Create your models here.
 class Partner(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, primary_key=id)
-    image = models.ImageField(upload_to='image/')
+    banner = models.ImageField(upload_to='image/')
     corporate_name = models.CharField(max_length=255)
     biography = models.TextField()
     verified = models.BooleanField(default=False)
@@ -20,3 +19,6 @@ class Partner(models.Model):
 
     def all_posts(self):
         return Post.objects.all().filter(author_id=self.user_id, for_cooperative=False)
+
+    def user_detail(self):
+        return User.objects.get(id=self.user_id)
--- partner/views.py
+++ partner/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
-from django.contrib import auth
+from core.models import User
 from partner.models import Partner
 from post.models import Post
 from Lists import Tag
@@ -20,23 +20,20 @@ def be_partner(request):
         spec = list(request.POST.getlist('spec', False))
         web = request.POST.get('web', False)
         if corp_name and bio and spec and web:
-            user = request.user
-            partner = Partner.objects.get(user_id=user.id)
-            if partner.DoesNotExist:
-                d_partner = Partner.objects.get(corporate_name=corp_name)
-                if d_partner.DoesNotExist:
+            user = User.objects.get(id=request.user.id)
+            try:
+                partner = Partner.objects.get(user_id=user.id)
+            except Partner.DoesNotExist:
+                try:
+                    d_partner = Partner.objects.get(corporate_name=corp_name)
+                except Partner.DoesNotExist:
                     m_partner = Partner()
                     m_partner.user = user
                     m_partner.website = web
-                    m_partner.image = request.user.image
                     m_partner.biography = bio
                     m_partner.corporate_name = corp_name
-                    real_spec = ''
-                    for val in spec:
-                        real_spec = val+';'
-                    m_partner.specialization = real_spec
+                    m_partner.specialization = spec
                     m_partner.save()
-                    user.save()
                     return redirect('home', {'message': 'You have successfully become a partner', 'status': 'success'})
                 else:
                     return render(request, 'partner/be_partner.html',
@@ -54,9 +51,7 @@ def be_partner(request):
 
 
 def partner_detail(request, id_):
-    partner = get_object_or_404(Partner, pk=id_)
-    partners = Partner.objects.all().order_by('-id')[:10][-1]
-    my_tags = partner.specialization
-    return render(request, 'partner/partner_detail.html', {'partner': partner, 'tags': my_tags, 'partners': partners})
+    partner = Partner.objects.get(user_id=id_)
+    return render(request, 'partner/partner_detail.html', {'partner': partner})
 
 
